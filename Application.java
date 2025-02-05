7d77763d4b4da0a6a67b62319d6c59c56123a968
yanokyo <23394769+yanokyo@users.noreply.github.com>
2025-02-05 02:25:11 +0000

    chore: fix linting issues

--- Application.java
+++ Application.java
@@ -8,38 +8,39 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class Application {
 
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
+
+  private void DoReallyNothing() {
+    try {
+      var counter = 100;
+
+    } catch (Exception e) {
+
     }
+  }
 
-    private void DoReallyNothing() {
-        try {
-            var counter = 100;
+  /**
+   * some java doc.
+   *
+   * @param ctx application contexxt
+   * @return some return
+   */
+  @Bean
+  public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+    return args -> {
+      System.out.println("Let's inspect the beans provided by Spring Boot:");
 
-        } catch (Exception e) {
+      var LongString =
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor"
+              + " incididunt ut labore et dolore magna aliqua. Porta lorem";
 
-        }
-    }
-
-    /**
-     * some java doc.
-     *
-     * @param ctx application contexxt
-     * @return some return
-     */
-    @Bean
-    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
-        return args -> {
-            System.out.println("Let's inspect the beans provided by Spring Boot:");
-
-            var LongString = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Porta lorem";
-
-            String[] beanNames = ctx.getBeanDefinitionNames();
-            Arrays.sort(beanNames);
-            for (String beanName : beanNames) {
-                System.out.println(beanName);
-            }
-        };
-    }
-
+      String[] beanNames = ctx.getBeanDefinitionNames();
+      Arrays.sort(beanNames);
+      for (String beanName : beanNames) {
+        System.out.println(beanName);
+      }
+    };
+  }
 }
